904d72c60e4053dd8686238b41e21f40f291ce7f
LH17674619240 <806934858@qq.com>
2024-10-10 15:31:36 +0800

    Update post, put and delete methods

--- JihangLiangA2-api/controllerAPI/api-controller.js
+++ JihangLiangA2-api/controllerAPI/api-controller.js
@@ -101,27 +101,96 @@ router.post("/addDonation", (req, res)=>{
 })
 
 router.post("/addFundraiser", (req, res)=>{
-	
-})
-
-router.put("/update", (req, res)=>{
-	
-})
-
-router.delete("delete/:id", (req, res)=>{
-    const query = `
-    SELECT f.FUNDRAISER_ID, f.ORGANIZER, f.CAPTION, f.TARGET_FUNDING, f.CURRENT_FUNDING, f.CITY, f.ACTIVE, c.NAME as CATEGORY_NAME 
-    FROM FUNDRAISER f
-    JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
-    WHERE f.FUNDRAISER_ID = 
-`;
-	connection.query(query + "delete from FUNDRAISER where FUNDRAISER_ID=" + req.params.id, (err, records,fields)=> {
+	var FUNDRAISER_ID = req.body.FUNDRAISER_ID;
+    var ORGANIZER = req.body.ORGANIZER;
+    var CAPTION = req.body.CAPTION;
+    var TARGET_FUNDING = req.body.TARGET_FUNDING;
+    var CURRENT_FUNDING = req.body.CURRENT_FUNDING;
+    var CITY = req.body.CITY;
+    var ACTIVE = req.body.ACTIVE;
+    connection.query("INSERT INTO DONATION VALUES("+FUNDRAISER_ID+","+ORGANIZER+","+CAPTION+","+TARGET_FUNDING+","+CURRENT_FUNDING+","+CITY+","+ACTIVE+")",
+	(err, result)=> {
 		 if (err){
-			 console.error("Error while deleting the data");
+			 console.error("Error while retrieve the data" + err);
 		 }else{
-			 res.send({delete:"Delete Sucess"});
+			 res.send({insert:"success"});
 		 }
 	})
 })
+
+router.put("/updateDonation", (req, res) => {
+    var DONATION_ID = req.body.DONATION_ID;
+    var FUNDRAISER_ID = req.body.FUNDRAISER_ID;
+    var DATE = req.body.DATE;
+    var AMOUNT = req.body.AMOUNT;
+    var GIVER = req.body.GIVER;
+
+    connection.query("UPDATE DONATION SET FUNDRAISER_ID="+FUNDRAISER_ID+", DATE="+DATE+", AMOUNT="+AMOUNT+", GIVER="+GIVER+" WHERE DONATION_ID="+DONATION_ID,
+    (err, result) => {
+        if (err) {
+            console.error("Error while updating the data: " + err);
+        } else {
+            res.send({update: "success"});
+        }
+    });
+});
+
+router.put("/updateFundraise/:id", (req, res) => {
+    var FUNDRAISER_ID = req.params.id;
+    var ORGANIZER = req.body.ORGANIZER;
+    var CAPTION = req.body.CAPTION;
+    var TARGET_FUNDING = req.body.TARGET_FUNDING;
+    var CURRENT_FUNDING = req.body.CURRENT_FUNDING;
+    var CITY = req.body.CITY;
+    var ACTIVE = req.body.ACTIVE;
+
+    connection.query(
+        "UPDATE FUNDRAISER SET ORGANIZER=?, CAPTION=?, TARGET_FUNDING=?, CURRENT_FUNDING=?, CITY=?, ACTIVE=? WHERE FUNDRAISER_ID=?", 
+        [ORGANIZER, CAPTION, TARGET_FUNDING, CURRENT_FUNDING, CITY, ACTIVE, FUNDRAISER_ID],
+    (err, result) => {
+        if (err) {
+            console.error("Error while updating the data: " + err);
+        } else {
+            res.send({update: "success"});
+        }
+    });
+});
+
+
+router.delete("/delete/:id", (req, res) => {
+    const FUNDRAISER_ID = req.params.id;
+    const selectQuery = `
+        SELECT f.FUNDRAISER_ID, f.ORGANIZER, f.CAPTION, f.TARGET_FUNDING, f.CURRENT_FUNDING, f.CITY, f.ACTIVE, c.NAME as CATEGORY_NAME 
+        FROM FUNDRAISER f
+        JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
+        WHERE f.FUNDRAISER_ID = ?
+    `;
+
+    connection.query(selectQuery, [FUNDRAISER_ID], (err, records) => {
+        if (err) {
+            console.error("Error while retrieving data: " + err);
+            res.send("Error while retrieving data.");
+        } else if (records.length === 0) {
+            res.send("Fundraiser not found.");
+        } else {
+
+            const fundraiserInformation = records[0];
+            
+            const deleteQuery = "DELETE FROM FUNDRAISER WHERE FUNDRAISER_ID = ?";
+            connection.query(deleteQuery, [FUNDRAISER_ID], (err, result) => {
+                if (err) {
+                    console.error("Error while deleting the data: " + err);
+                    res.send("Error while deleting the fundraiser.");
+                } else {
+                    res.send({
+                        delete: "Delete Success",
+                        deletedFundraiser: fundraiserInformation
+                    });
+                }
+            });
+        }
+    });
+});
+
 
 module.exports = router;
